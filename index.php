c716cb633fe1e7d7c6292a49f5c3d438d3250803
nael10 <n.nael@hotmail.com>
2018-04-11 10:01:40 +0200

    adding two more files

--- index.php
+++ index.php
@@ -8,3 +8,15 @@ $log->pushHandler(new StreamHandler('greetings.log', Logger::INFO));
 $myString = "hello ".$argv[1]." what a wonderfull name\n";
 echo "Hello ".$argv[1]." What a wonderfull name\n";
 $log->info("$myString");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Testing, Testing>/title>
+</head>
+<body>
+<?php
+echo "hello, " . $argv[1];
+?>
+</body>
+</html>
